1d4026b816d4c0bba2f2a4e4044b5faf9c2c4401
Александр Тимченко <timon66.98@mail.ru>
2026-04-17 23:55:33 +0300

    -

--- src/ru/yandex/practicum/WordleDictionary.java
+++ src/ru/yandex/practicum/WordleDictionary.java
@@ -1,14 +1,48 @@
 package ru.yandex.practicum;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
-/*
-этот класс содержит в себе список слов List<String>
-    его методы похожи на методы списка, но учитывают особенности игры
-    также этот класс может содержать рутинные функции по сравнению слов, букв и т.д.
- */
 public class WordleDictionary {
 
-    private List<String> words;
+    private ArrayList<String> words = new ArrayList<>();
+    private final Random random = new Random();
+
+    public void loadDictionary(String filePath) {
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(filePath), StandardCharsets.UTF_8));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String word = normalize(line);
+                if (word.length() == 5) {
+                    words.add(word);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("Произошла ошибка,файл не найден");
+        } catch (IOException e) {
+            throw new RuntimeException("Во-время обработки слов произошла непредвиденная ошибка");
+        }
+        System.out.println("Успешная обработка словаря");
+    }
+    public String normalize(String word) {
+        if (word == null) {
+            return "";
+        }
+        return word.trim().toLowerCase().replace('ё',  'е');
+    }
+    public String getRandomWord() {
+        if (words.isEmpty()) {
+            return words.get(random.nextInt(0, words.size()));
+        }
+        throw new RuntimeException("Данный файл не найден");
+    }
+
+    public ArrayList<String> getWords() {
+        return words;
+    }
 
 }
--- src/ru/yandex/practicum/WordleGame.java
+++ src/ru/yandex/practicum/WordleGame.java
@@ -1,17 +1,7 @@
 package ru.yandex.practicum;
 
-/*
-в этом классе хранится словарь и состояние игры
-    текущий шаг
-    всё что пользователь вводил
-    правильный ответ
+import java.util.*;
 
-в этом классе нужны методы, которые
-    проанализируют совпадение слова с ответом
-    предложат слово-подсказку с учётом всего, что вводил пользователь ранее
-
-не забудьте про специальные типы исключений для игровых и неигровых ошибок
- */
 public class WordleGame {
 
     private String answer;
@@ -20,4 +10,93 @@ public class WordleGame {
 
     private WordleDictionary dictionary;
 
+    private boolean isWon;
+
+    LinkedHashMap<String, String> history;
+
+    public WordleGame(WordleDictionary dictionary) {
+        this.dictionary = dictionary;
+        this.answer = dictionary.getRandomWord();
+        this.steps = 6;
+        isWon = false;
+        this.history = new LinkedHashMap<>();
+    }
+
+    public String makeStep(String guess) {
+        if (isWon || steps <= 0 ) {
+            throw new RuntimeException("Игра окончена");
+
+        }
+        String normalGuess = guess.toLowerCase().replace('ё', 'е');
+        String resultMask = checkWorld(normalGuess);
+        history.put(normalGuess, resultMask);
+        steps--;
+        if (normalGuess.equals(answer)) {
+            isWon = true;
+        }
+        return resultMask;
+        
+    }
+
+    public String getHint() {
+        List<String> hint = new ArrayList<>();
+        for (String word : dictionary.getWords()) {
+            if (matchHistory(word)) {
+                hint.add(word);
+            }
+        }
+        if (hint.isEmpty()) {
+           return "Подсказок нет";
+        }
+        Random random = new Random();
+        return hint.get(random.nextInt(hint.size()));
+    }
+    private boolean matchHistory(String word) {
+        for (Map.Entry<String, String> entry : history.entrySet()) {
+          String guessWord = entry.getKey();
+          String expected = entry.getValue();
+          if (!machMask(word, guessWord, expected)) {
+              return false;
+          }
+        }
+        return true;
+    }
+    private boolean machMask(String word, String guess, String mask) {
+
+    }
+
+    private String checkWorld(String guess) {
+        char[] mask = new char[5];
+        Arrays.fill(mask, '-');
+        char[] targetChars = answer.toCharArray();
+        char[] guessChars = guess.toCharArray();
+        for (int i = 0; i < 5; i++) {
+            if (guessChars[i] == targetChars[i]) {
+                mask[i] = '+';
+                targetChars[i] = '\0';
+                guessChars[i] = '\0';
+
+            }
+        }
+        for (int i = 0; i < 5; i++) {
+            if (guessChars[i] == '\0') {
+                continue;
+            }
+            boolean found = false;
+
+            for (int j = 0; j < 5; j++) {
+                if (targetChars[i] == guessChars[i]) {
+                    mask[i] = '^';
+                    targetChars[j] = '\0';
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                mask[i] = '-';
+
+            }
+        }
+        return new String(mask);
+    }
 }
