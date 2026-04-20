6ba78a2480db8f53a4de13cdfd5027377f92283f
Александр Тимченко <timon66.98@mail.ru>
2026-04-20 19:10:53 +0300

    -

--- test/ru/yandex/practicum/WordleTest.java
+++ test/ru/yandex/practicum/WordleTest.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 class WordleTest {
 
     private WordleDictionary createDictionary(List<String> wordsList) throws IOException {
